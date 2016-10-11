df532239cce6118f1d2372ea2316215263b34a3b
Serghei Luchianenco <serghei.luchianenco@freshcells.de>
2016-10-11 22:31:30 +0200

    December holidays

--- src/Data/Month/En/Us/December.php
+++ src/Data/Month/En/Us/December.php
@@ -3,8 +3,18 @@ namespace Skybluesofa\OnThisDay\Data\Month\En\Us;
 
 use Skybluesofa\OnThisDay\Data\Contract\Month;
 
-class December extends Month {
-    public static $recurringEvents = [];
+class December extends Month
+{
+    public static $recurringEvents = [
+        '6' => ['St Nicholas\' Day'],
+        '7' => ['Pearl Harbor Remembrance Day'],
+        '13' => ['National Guard Birthday'],
+        '17' => ['Pan American Aviation Day', 'Wright Brothers Day'],
+        '24' => ['Christmas Eve'],
+        '25' => ['Christmas'],
+        '26' => ['Day After Christmas Day'],
+        '31' => ['New Year\'s Eve']
+    ];
 
     public static $specificDateEvents = [];
 
@@ -14,12 +24,14 @@ class December extends Month {
 
     public static $configurationHolidays = [];
 
-    protected function getRecurringAdvancedConfigurationBasedEvents(\Carbon\Carbon $date) {
+    protected function getRecurringAdvancedConfigurationBasedEvents(\Carbon\Carbon $date)
+    {
         $events = [];
         return $events;
     }
 
-    protected function getRecurringAdvancedConfigurationBasedHolidays(\Carbon\Carbon $date) {
+    protected function getRecurringAdvancedConfigurationBasedHolidays(\Carbon\Carbon $date)
+    {
         $events = [];
         return $events;
     }
