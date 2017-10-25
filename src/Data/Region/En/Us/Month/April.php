5d739700f2bb15c0e9d37bf88a3f024d10d9dd7a
i.shugaev <i.shugaiev@levi9.com>
2017-10-25 10:09:50 +0300

    Set up recurring events for April #hactoberfest

--- src/Data/Region/En/Us/Month/April.php
+++ src/Data/Region/En/Us/Month/April.php
@@ -6,7 +6,37 @@ use Skybluesofa\OnThisDay\Data\Region\En\Us\Helpers\Easter;
 use Carbon\Carbon;
 
 class April extends Month {
-    public static $recurringEvents = [];
+    public static $recurringEvents = [
+        "1"  => [],
+        "2"  => ["Pascua Florida Day"],
+        "3"	 => ["Pascua Florida Day observed"],
+        "4"  => [],
+        "5"  => [],
+        "6"	 => ["National Tartan Day"],
+        "7"  => [],
+        "8"  => [],
+        "9"  => ["Palm Sunday"],
+        "11" => ["Passover (first day)", "National Library Workers' Day"],
+        "12" => [],
+        "13" => ["Maundy Thursday", "Thomas Jefferson's Birthday"],
+        "14" => ["Orthodox Good Friday", "Good Friday"],
+        "15" => ["Holy Saturday", "Orthodox Holy Saturday", "Father Damien Day"],
+        "16" => ["Orthodox Easter", "Easter Sunday", "Emancipation Day"],
+        "17" => ["Orthodox Easter Monday", "Easter Monday", "Emancipation Day observed", "Patriot's Day"],
+        "18" => ["Last Day of Passover", "Tax Day	Observance"],
+        "19" => [],
+        "20" => [],
+        "21" => ["San Jacinto Day"],
+        "22" => ["Oklahoma Day"],
+        "23" => [],
+        "24" => ["Yom HaShoah", "Isra and Mi'raj", "Confederate Memorial Day", "State Holiday Georgia", "State holiday Mississippi"],
+        "25" => [],
+        "26" => ["State Holiday	Florida", "Administrative Professionals Day"],
+        "27" => ["Take our Daughters and Sons to Work Day"],
+        "28" => ["Arbor Day"],
+        "29" => [],
+        "30" => []
+    ];
 
     public static $specificDateEvents = [];
 
