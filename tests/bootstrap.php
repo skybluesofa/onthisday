6d4e0e22810f8993ea74dfec119f42d16c78f1e0
Dave Rogers <connect@skybluesofa.com>
2018-07-13 13:42:50 -0500

    Travis update…again

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,5 @@
 <?php
-//print __DIR__;die();
-require __DIR__.'/../../../autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 $classLoader = new \Composer\Autoload\ClassLoader();
 $classLoader->addPsr4("Skybluesofa\\OnThisDayTest\\", __DIR__, true);
